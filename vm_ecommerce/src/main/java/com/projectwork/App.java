89d8fa7fd5beca74ca8909edf33d90d37bd8bc6e
MicheleDanza <michele-danza@hotmail.it.com>
2025-03-13 09:36:11 +0100

    Inizio projectwork di java

---
+++ vm_ecommerce/src/main/java/com/projectwork/App.java
@@ -0,0 +1,13 @@
+package com.projectwork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
